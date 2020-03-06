31ae0afdb78ff4e618f482c052efed7f9fdcd7b9
liuweixian <65506466@qq.com>
2020-03-06 17:08:56 +0800

    SceneView切换

---
+++ VisGenerator/Assets/Scripts/SceneMananger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneView
+{
+    Invliad,
+    ASView,
+    IPView,
+    MapView,
+}
+
+public class SceneMananger : MonoBehaviour
+{
+    private static SceneMananger ms_instance = null;
+
+    public static SceneMananger Instance
+    {
+        get
+        {
+            if (ms_instance == null)
+            {
+                GameObject obj = new GameObject("SceneManager");
+                GameObject parent = GameObject.Find("Singleton");
+                if (parent != null)
+                    obj.transform.parent = parent.transform;
+                ms_instance = obj.AddComponent<SceneMananger>();
+            }
+
+            return ms_instance;
+        }
+    }
+
+    private SceneView currentSceneView = SceneView.Invliad;
+
+    public SceneView CurrentSceneView
+    {
+        get { return currentSceneView; }
+    }
+
+    private Dictionary<SceneView, GameObject> viewToObjMapping = null;
+
+    private void Awake()
+    {
+        ms_instance = this;
+        viewToObjMapping = new Dictionary<SceneView, GameObject>();
+        viewToObjMapping.Add(SceneView.ASView, GameObject.Find("CameraParent-AS"));
+        viewToObjMapping.Add(SceneView.IPView, GameObject.Find("CameraParent-IP"));
+        viewToObjMapping.Add(SceneView.MapView, GameObject.Find("CameraParent-Map"));
+        
+        ChangeScene(SceneView.ASView);
+    }
+
+    public void ChangeScene(SceneView targetSceneView)
+    {
+        if (currentSceneView == targetSceneView)
+            return;
+        currentSceneView = targetSceneView;
+        foreach (var pair in viewToObjMapping)
+        { 
+            pair.Value.SetActive(pair.Key == currentSceneView);
+        }
+    }
+}
---
+++ VisGenerator/Assets/Scripts/UI/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewController : MonoBehaviour
+{
+    void Start()
+    {
+        Toggle[] toggleList = this.GetComponentsInChildren<Toggle>();
+        for (int i = 0; i < toggleList.Length; i++)
+        {
+            Toggle toggle = toggleList[i];
+            toggle.onValueChanged.AddListener(delegate { 
+                OnToggleValueChanged(toggle); 
+            });
+        }
+    }
+
+    private void OnToggleValueChanged(Toggle toggle)
+    {
+        if (!toggle.isOn)
+            return;
+        
+        switch (toggle.gameObject.name)
+        {
+            case "ASView":
+                SceneMananger.Instance.ChangeScene(SceneView.ASView);
+                break;
+            case "IPView":
+                SceneMananger.Instance.ChangeScene(SceneView.IPView);
+                break;
+            case "MapView":
+                SceneMananger.Instance.ChangeScene(SceneView.MapView);
+                break;
+        }
+    }
+}
