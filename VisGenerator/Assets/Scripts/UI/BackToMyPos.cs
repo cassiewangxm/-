9b31f327dbf4b0e8b4224d881b0c027af342d80c
liuweixian <65506466@qq.com>
2020-03-06 17:51:30 +0800

    加入BackToMyPos按钮响应

---
+++ VisGenerator/Assets/Scripts/UI/BackToMyPos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackToMyPos : MonoBehaviour
+{
+    private void Awake()
+    {
+        Button btn = this.GetComponentInChildren<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        //test code
+        SceneMananger.Instance.ChangeScene(SceneView.MapView);
+    }
+}
--- VisGenerator/Assets/Scripts/UI/ViewController.cs
+++ VisGenerator/Assets/Scripts/UI/ViewController.cs
@@ -1,19 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ViewController : MonoBehaviour
 {
+    private Toggle[] toggleList = null;
     void Start()
     {
-        Toggle[] toggleList = this.GetComponentsInChildren<Toggle>();
+        toggleList = this.GetComponentsInChildren<Toggle>();
         for (int i = 0; i < toggleList.Length; i++)
         {
             Toggle toggle = toggleList[i];
             toggle.onValueChanged.AddListener(delegate { 
                 OnToggleValueChanged(toggle); 
             });
+        }
+        EventManager.RegistEvent(EventDefine.OnSceneViewChange, (Action)OnSceneChange);
+    }
+
+    private void OnSceneChange()
+    {
+        switch (SceneMananger.Instance.CurrentSceneView)
+        {
+            case SceneView.ASView:
+                RefreshToggleList("ASView");
+                break;
+            case SceneView.MapView:
+                RefreshToggleList("MapView");
+                break;
+            case SceneView.IPView:
+                RefreshToggleList("IPView");
+                break;
+        }
+    }
+
+    private void RefreshToggleList(string selectedToggle)
+    {
+        for (int i = 0; i < toggleList.Length; i++)
+        {
+            Toggle toggle = toggleList[i];
+            toggle.isOn = toggle.gameObject.name == selectedToggle;
         }
     }
 
