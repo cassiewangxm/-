b76eda138a745a843b92d1222dad3990d320c1b6
liuweixian <65506466@qq.com>
2020-03-09 10:35:22 +0800

    修复正常的as visual effect的bounds

--- VisGenerator/Assets/Scripts/Consts.cs
+++ VisGenerator/Assets/Scripts/Consts.cs
@@ -9,21 +9,15 @@ public class Consts : MonoBehaviour
     public Vector2 ASPos;
     public Vector2 IPSize;
     public Vector2 IPPos;
-    public Vector2 MapSize;
-    public Vector2 MapPos;
+    public Vector3 MapSize;
+    public Vector3 MapPos;
 
     public GameObject ASGameObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        ASGameObject.GetComponent<VisualEffect>().SetVector3("position", new Vector3(ASPos.x, 0.0f, ASPos.y));
-        ASGameObject.GetComponent<VisualEffect>().SetVector3("size", new Vector3(ASSize.x, 0.0f, ASSize.y));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ASGameObject.GetComponent<VisualEffect>().SetVector3("position", MapPos);
+        ASGameObject.GetComponent<VisualEffect>().SetVector3("size", MapSize);
     }
 }
