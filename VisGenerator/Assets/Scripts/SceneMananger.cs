cf33027d9d7856ad66a0f46d282d10a539739daa
liuweixian <65506466@qq.com>
2020-03-11 13:39:23 +0800

    切换场景的过渡动画

--- VisGenerator/Assets/Scripts/SceneMananger.cs
+++ VisGenerator/Assets/Scripts/SceneMananger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RTG;
@@ -35,6 +36,9 @@ public class SceneMananger : MonoBehaviour
     }
 
     private SceneView currentSceneView = SceneView.Invliad;
+    private SceneView lastSceneView = SceneView.Invliad;
+    private float changeSceneTweenTime = 0.3f;
+    private float curChangeSceneTweenTime = 0f;
 
     public SceneView CurrentSceneView
     {
@@ -54,19 +58,54 @@ public class SceneMananger : MonoBehaviour
         ChangeScene(SceneView.ASView);
     }
 
-    public void ChangeScene(SceneView targetSceneView)
+    public void ChangeScene(SceneView targetSceneView, bool doTween = true)
     {
         if (currentSceneView == targetSceneView)
             return;
+
+        lastSceneView = currentSceneView;
         currentSceneView = targetSceneView;
+        curChangeSceneTweenTime = doTween ? 0 : changeSceneTweenTime;
+        
         foreach (var pair in viewToObjMapping)
         { 
-            pair.Value.SetActive(pair.Key == currentSceneView);
-            if (pair.Key == currentSceneView)
-            {
+            pair.Value.SetActive(pair.Key == currentSceneView || pair.Key == lastSceneView);
+            if (pair.Key == currentSceneView) 
                 RTFocusCamera.SetTargetCamera(pair.Value.GetComponentInChildren<Camera>());
-            }
         }
         EventManager.SendEvent(EventDefine.OnSceneViewChange);
+    }
+
+    private void Update()
+    {
+        if (lastSceneView == currentSceneView || lastSceneView == SceneView.Invliad)
+            return;
+        
+        GameObject lastSceneViewObj = viewToObjMapping[lastSceneView];
+        Camera lastSceneViewCamera = lastSceneViewObj.GetComponentInChildren<Camera>();
+        GameObject curSceneViewObj = viewToObjMapping[currentSceneView];
+        Camera curSceneViewCamera = curSceneViewObj.GetComponentInChildren<Camera>();
+
+        curChangeSceneTweenTime += Time.deltaTime;
+        bool finishTween = curChangeSceneTweenTime >= changeSceneTweenTime;
+        int offset = lastSceneView - currentSceneView;
+        if (finishTween)
+        {
+            curChangeSceneTweenTime = changeSceneTweenTime;
+            lastSceneView = currentSceneView;
+            lastSceneViewObj.SetActive(false);
+        }
+        
+        float tmp = Mathf.Lerp(0, 1, curChangeSceneTweenTime / changeSceneTweenTime);
+        if (offset < 0)
+        {
+            lastSceneViewCamera.rect = new Rect(0, 0, 1 - tmp, 1);
+            curSceneViewCamera.rect = new Rect(1 - tmp, 0, tmp, 1);
+        }
+        else
+        {
+            lastSceneViewCamera.rect = new Rect(tmp, 0, 1 - tmp, 1);
+            curSceneViewCamera.rect = new Rect(0, 0, tmp, 1);
+        }
     }
 }
