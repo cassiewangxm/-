e03018066b085eb3f7dc42acd44e218609e0172a
liuweixian <65506466@qq.com>
2020-03-17 14:25:54 +0800

    只有在处理点击的时候才readback texture

--- VisGenerator/Assets/Scripts/ASViewRaycast.cs
+++ VisGenerator/Assets/Scripts/ASViewRaycast.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
@@ -9,25 +10,25 @@ using UnityEngine.Windows;
 public class ASViewRaycast : MonoBehaviour
 {
     public Filters ASFilter = null;
-    
+
     private Texture2D asRaycastTexture = null;
     private Texture2D asRaycastReadbackTexture = null;
     private int curInputX = -1;
     private int curInputY = -1;
     private int curScreenWidth = 0;
     private int curScreenHeight = 0;
-    private Vector3 curCameraPosition = Vector3.zero;
-    private Quaternion curCameraRotation = Quaternion.Euler(Vector3.one);
+    private Color[] clearColors = null;
+    public bool usingAsync = false;
 
     private void Awake()
     {
         InitOrResizeASRaycastTexture();
-        
+
         GameObject cameraObj = GameObject.Find("CameraParent-AS");
         Camera curCamera = cameraObj.GetComponentInChildren<Camera>();
 
         VisualEffect ve = this.GetComponent<VisualEffect>();
-        
+
         RenderTextureDescriptor rtd = new RenderTextureDescriptor();
         rtd.sRGB = false;
         rtd.volumeDepth = 1;
@@ -39,70 +40,109 @@ public class ASViewRaycast : MonoBehaviour
 
         RenderPipelineManager.beginFrameRendering += (context, camera) =>
         {
-            // bool screenSizeChange = curScreenWidth != Screen.width || curScreenHeight != Screen.height;
-            // bool cameraPosChange = (curCameraPosition != curCamera.gameObject.transform.position) ||
-            //                        (curCameraRotation != curCamera.gameObject.transform.rotation);
-            //
-            // if (screenSizeChange || cameraPosChange || asRaycastReadbackTexture == null)
-            {
-                Texture texture = ve.GetTexture("RaycastTexture");
-                if (texture == null)
-                    return;
+            Texture texture = ve.GetTexture("RaycastTexture");
+            if (texture == null)
+                return;
 
+            AsyncGPUReadback.Request(texture, 0, null);
+            if (curInputX != -1 && curInputY != -1)
+            {
                 rtd.width = texture.width;
                 rtd.height = texture.height;
 
-                RenderTexture renderTexture = RenderTexture.GetTemporary(rtd);
-                Graphics.Blit(texture, renderTexture);
-
-                if (asRaycastReadbackTexture == null)
-                    asRaycastReadbackTexture = new Texture2D(texture.width, texture.height, TextureFormat.ARGB32, false, true);
-                else if (asRaycastReadbackTexture.width != texture.width || asRaycastReadbackTexture.height != texture.height)
-                    asRaycastReadbackTexture.Resize(texture.width, texture.height);
-                RenderTexture.active = renderTexture;
-                asRaycastReadbackTexture.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
-                asRaycastReadbackTexture.Apply();
-                RenderTexture.ReleaseTemporary(renderTexture);
-
-                // curScreenWidth = texture.width;
-                // curScreenHeight = texture.height;
-                // curCameraPosition = curCamera.gameObject.transform.position;
-                // curCameraRotation = curCamera.gameObject.transform.rotation;
-                // Debug.Log("--->" + screenSizeChange + "," + cameraPosChange);
+                if (usingAsync)
+                    AsyncGPUReadback.Request(texture, 0, AsyncGPUReadbackCallback);
+                else
+                    SyncGPUReadback(rtd, texture);
             }
 
-            if (curInputX != -1 && curInputY != -1)
-            {
-                Color color = asRaycastReadbackTexture.GetPixel(curInputX, curInputY);
-                if (color.b > 0)
-                    return;
-
-                int indexX = (int) Math.Round(color.r * 256);
-                int indexY = (int) Math.Round(color.g * 256);
-                ASFilter.FilterBySelectedAS(indexX, indexY);
-
-                // Debug.Log("---->" + curInputX + "-" + curInputY + "Color --> " + color);
-                // Debug.Log("------>" + indexX + "," + indexY);
-            }
-
-            // if (screenSizeChange || cameraPosChange)
-            // {
             InitOrResizeASRaycastTexture();
-            // }
         };
+    }
+
+    private void SyncGPUReadback(RenderTextureDescriptor rtd, Texture texture)
+    {
+        RenderTexture renderTexture = RenderTexture.GetTemporary(rtd);
+        Graphics.Blit(texture, renderTexture);
+
+        if (asRaycastReadbackTexture == null)
+            asRaycastReadbackTexture = new Texture2D(texture.width, texture.height, TextureFormat.ARGB32, false, true);
+        else if (asRaycastReadbackTexture.width != texture.width || asRaycastReadbackTexture.height != texture.height)
+            asRaycastReadbackTexture.Resize(texture.width, texture.height);
+        RenderTexture.active = renderTexture;
+        asRaycastReadbackTexture.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
+        asRaycastReadbackTexture.Apply();
+        RenderTexture.ReleaseTemporary(renderTexture);
+
+        Color color = asRaycastReadbackTexture.GetPixel(curInputX, curInputY);
+        if (color.b > 0)
+        {
+            curInputX = -1;
+            curInputY = -1;
+            return;
+        }
+
+        int indexX = (int) Math.Round(color.r * 256);
+        int indexY = (int) Math.Round(color.g * 256);
+        ASFilter.FilterBySelectedAS(indexX, indexY);
+
+        Debug.Log("Sync---->" + curInputX + "-" + curInputY + "Color --> " + color);
+        Debug.Log("Sync------>" + indexX + "," + indexY);
+
+        curInputX = -1;
+        curInputY = -1;
+    }
+
+    private void AsyncGPUReadbackCallback(AsyncGPUReadbackRequest request)
+    {
+        if (!request.done)
+            return;
+        if (curInputX == -1 || curInputY == -1)
+            return;
+        NativeArray<Color32> colors = request.GetData<Color32>();
+        Color32 color = colors[curInputY * request.width + curInputY];
+        Debug.Log("AsyncGPUReadbackCallback" + color);
+        if (color.b > 0)
+        {
+            curInputX = -1;
+            curInputY = -1;
+            return;
+        }
+
+        int indexX = color.r;
+        int indexY = color.g;
+        ASFilter.FilterBySelectedAS(indexX, indexY);
+
+        Debug.Log("Async---->" + curInputX + "-" + curInputY + "Color --> " + color);
+        Debug.Log("Async------>" + indexX + "," + indexY);
+
+        curInputX = -1;
+        curInputY = -1;
     }
 
     private void InitOrResizeASRaycastTexture()
     {
+        bool needClearColor = false;
         if (asRaycastTexture == null)
+        {
             asRaycastTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false, true);
-        else
+            needClearColor = true;
+        }
+        else if (asRaycastTexture.width != Screen.width || asRaycastTexture.height != Screen.height)
+        {
             asRaycastTexture.Resize(Screen.width, Screen.height);
-        int totalColor = Screen.width * Screen.height;
-        Color[] colors = new Color[totalColor];
-        for (int i = 0; i < totalColor; i++)
-            colors[i] = new Color(0, 0, 1, 0);
-        asRaycastTexture.SetPixels(colors);
+            needClearColor = true;
+        }
+
+        if (needClearColor)
+        {
+            int totalColor = Screen.width * Screen.height;
+            clearColors = new Color[totalColor];
+            for (int i = 0; i < totalColor; i++)
+                clearColors[i] = new Color(0, 0, 1, 0);
+        }
+
+        asRaycastTexture.SetPixels(clearColors);
         asRaycastTexture.Apply();
         VisualEffect ve = this.GetComponent<VisualEffect>();
         ve.SetTexture("RaycastTexture", asRaycastTexture);
@@ -115,10 +155,11 @@ public class ASViewRaycast : MonoBehaviour
             curInputX = (int) Input.mousePosition.x;
             curInputY = (int) Input.mousePosition.y;
         }
-        else
-        {
-            curInputX = -1;
-            curInputY = -1;
-        }
+    }
+
+    private void OnDestroy()
+    {
+        VisualEffect ve = this.GetComponent<VisualEffect>();
+        ve.SetTexture("RaycastTexture", null);
     }
 }
